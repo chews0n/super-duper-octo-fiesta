41c17ad1a7480e5e9879be567ebc5faff7f9f71c
chewson <chris@resfrac.com>
2022-03-18 14:03:39 -0600

    adding dca code for the first time

---
+++ decline_curve_analysis/dca.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from scipy.optimize import curve_fit
+import numpy as np
+import matplotlib
+
+matplotlib.use('Agg')  # use a different matplotlib backend
+
+import matplotlib.pyplot as plt
+
+plt.ioff()
+
+
+def main():
+    # this is the start of the main function
+    print("I am in main....")
+
+
+if __name__ == "__main__":
+    # This is the start of the running portion of the script executable
+    main()
+
